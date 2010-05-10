2ae9074ae4123861b1b40ea12efc9fa92cb16aa7
Theo Hultberg <theo@reventon.local>
2010-05-10 08:30:11 +0200

    Scala 2.8.0 RC1 + updated formula

--- Library/Formula/scala.rb
+++ Library/Formula/scala.rb
@@ -1,14 +1,27 @@
 require 'formula'
 
 class Scala <Formula
-  @homepage='http://www.scala-lang.org/'
-  @url='http://www.scala-lang.org/downloads/distrib/files/scala-2.7.7.final.tgz'
-  @version='2.7.7'
-  @md5='5d2294d5aab72fec869c0ba666d28b7e'
+  homepage 'http://www.scala-lang.org/'
+  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.8.0.RC1.tgz'
+  version '2.8.0.RC1'
+  md5 '920f1e817768acfed6cb874db0f86737'
+
+  def shim_script target
+    <<-EOS.undent
+      #!/bin/sh
+      exec #{libexec}/bin/#{target} $*
+    EOS
+  end
 
   def install
-    man1.install Dir['man/man1/*']
-    FileUtils.rm_f Dir["bin/*.bat"]
-    prefix.install Dir['*']
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    man.install Dir['man/*']
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |b|
+      n = Pathname.new(b).basename
+      (bin+n).write shim_script(n)
+    end
   end
+  
 end
